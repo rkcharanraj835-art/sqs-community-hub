d4ff85fac03310ec06957ba2ee494fc3f06bf56a
Charan <rkcharanraj835@gmail.com>
2026-07-06 22:22:52 +0530

    Admin added-2

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+import { auth } from "../../firebase/firebase";
 
 import Sidebar from "../../components/admin/Sidebar";
 import Dashboard from "../../components/admin/Dashboard";
@@ -9,6 +13,41 @@ import Settings from "../../components/admin/Settings";
 
 function Admin() {
   const [page, setPage] = useState("dashboard");
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      navigate("/admin");
+      return;
+    }
+
+    const allowedEmails = [
+      "rkcharanraj835@gmail.com",
+      "gnanavelv123@gmail.com",
+    ];
+
+    if (!allowedEmails.includes(user.email)) {
+      signOut(auth);
+      navigate("/admin");
+      return;
+    }
+
+    setLoading(false);
+  });
+
+  return () => unsubscribe();
+}, [navigate]);
+
+  if (loading) {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#070B14] text-white text-2xl">
+      Checking access...
+    </div>
+  );
+}
 
   return (
     <div className="flex min-h-screen bg-[#070B14]">
